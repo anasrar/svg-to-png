908744e7ba3316cf355c9b2b15b04aa71c358255
anasrar <mynameanasrar@gmail.com>
2022-10-30 23:06:05 +1300

    fix: svg dimension and `foreignObject` bug in chrome
    - svg does not have `width` and `height` or `viewBox`
    - using base64 instead blob to fix `foreignObject`
    - disable draggable on image preview
    - add notification dependency

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ import {
 } from "@mantine/core";
 import { Dropzone, FileWithPath } from "@mantine/dropzone";
 import {
+	NotificationsProvider,
+	showNotification,
+} from "@mantine/notifications";
+import {
+	IconFileAlert,
 	IconFileDownload,
 	IconPhoto,
 	IconPhotoPlus,
@@ -24,25 +29,47 @@ const App: FC = () => {
 	const refImage = useRef<HTMLImageElement>(null);
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	const refOpenFile = useRef(() => {});
-	const [blobUrl, setBlobUrl] = useState("");
+	const [svgBase64Url, setSvgBase64Url] = useState("");
 	const [fileName, setFileName] = useState("");
 	const [dimension, setDimension] = useState("");
 	const [colorBg, setColorBg] = useState("#aaaaaa");
 
 	const onOpenFile = async (files: FileWithPath[]) => {
 		const file = files[0];
-		const buffer = await file.arrayBuffer();
-		const blob = new Blob([new Uint8Array(buffer)], { type: file.type });
-		const url = URL.createObjectURL(blob);
+
+		// validate svg has width and height or viewBox
+		const elDummy = document.createElement("div");
+		elDummy.innerHTML = await file.text();
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		const elSvg = elDummy.querySelector("svg")!;
+		const hasWidth = elSvg.hasAttribute("width");
+		const hasHeight = elSvg.hasAttribute("height");
+		const hasViewBox = elSvg.hasAttribute("viewBox");
+
+		if (!hasWidth && !hasHeight && !hasViewBox) {
+			showNotification({
+				color: "red",
+				icon: <IconFileAlert size={18} />,
+				radius: "lg",
+				title: "SVG File Error",
+				message: "SVG does not contain attribute width and height or viewBox",
+				autoClose: 10000,
+			});
+			return;
+		}
+
+		if (!hasWidth && !hasHeight && hasViewBox) {
+			const { viewBox } = elSvg;
+			const { width, height } = viewBox.baseVal;
+			elSvg.setAttribute("width", width.toString());
+			elSvg.setAttribute("height", height.toString());
+		}
 
 		setFileName(file.name);
 
-		setBlobUrl((current) => {
-			if (current !== "") {
-				URL.revokeObjectURL(current);
-			}
-			return url;
-		});
+		setSvgBase64Url(
+			"data:image/svg+xml;base64," + window.btoa(elSvg.outerHTML)
+		);
 	};
 
 	const onImageLoaded = () => {
@@ -67,151 +94,155 @@ const App: FC = () => {
 			withGlobalStyles
 			withNormalizeCSS
 		>
-			<canvas
-				ref={refCanvas}
-				style={{
-					position: "absolute",
-					top: 0,
-					left: 0,
-					transform: "scale(0)",
-					pointerEvents: "none",
-				}}
-			></canvas>
-
-			<a ref={refDownload} style={{ display: "none" }}></a>
-
-			<Container size="sm" px="xs">
-				<Stack
-					spacing="sm"
-					py="xs"
+			<NotificationsProvider>
+				<canvas
+					ref={refCanvas}
 					style={{
-						display: "flex",
-						minHeight: "100vh",
+						position: "absolute",
+						top: 0,
+						left: 0,
+						transform: "scale(0)",
+						pointerEvents: "none",
 					}}
-				>
-					<Dropzone.FullScreen
-						accept={["image/svg+xml"]}
-						onDrop={onOpenFile}
-						openRef={refOpenFile}
-					>
-						<div></div>
-					</Dropzone.FullScreen>
+				></canvas>
 
-					<Dropzone
-						accept={["image/svg+xml"]}
-						onDrop={onOpenFile}
+				<a ref={refDownload} style={{ display: "none" }}></a>
+
+				<Container size="sm" px="xs">
+					<Stack
+						spacing="sm"
+						py="xs"
 						style={{
-							display: blobUrl === "" ? "flex" : "none",
-							flexGrow: 1,
-							justifyContent: "center",
-							alignItems: "center",
+							display: "flex",
+							minHeight: "100vh",
 						}}
 					>
-						<Group
-							position="center"
-							spacing="xs"
-							style={{ pointerEvents: "none" }}
+						<Dropzone.FullScreen
+							accept={["image/svg+xml"]}
+							onDrop={onOpenFile}
+							openRef={refOpenFile}
 						>
-							<Dropzone.Accept>
-								<IconUpload size={50} stroke={1.5} />
-							</Dropzone.Accept>
-							<Dropzone.Reject>
-								<IconX size={50} stroke={1.5} />
-							</Dropzone.Reject>
-							<Dropzone.Idle>
-								<IconPhoto size={50} stroke={1.5} />
-							</Dropzone.Idle>
+							<div></div>
+						</Dropzone.FullScreen>
 
-							<div>
-								<Text size="xl" inline>
-									Drag svg here or click to select svg file
-								</Text>
-							</div>
-						</Group>
-					</Dropzone>
-
-					<div
-						style={{
-							position: "relative",
-							display: blobUrl !== "" ? "block" : "none",
-							flexGrow: 1,
-							background: colorBg,
-						}}
-					>
-						<img
-							ref={refImage}
-							src={blobUrl}
+						<Dropzone
+							accept={["image/svg+xml"]}
+							onDrop={onOpenFile}
 							style={{
-								position: "absolute",
-								top: 0,
-								left: 0,
-								width: "100%",
-								height: "100%",
-								objectFit: "scale-down",
-								objectPosition: "center",
+								display: svgBase64Url === "" ? "flex" : "none",
+								flexGrow: 1,
+								justifyContent: "center",
+								alignItems: "center",
 							}}
-							onLoad={onImageLoaded}
-						/>
-					</div>
+						>
+							<Group
+								position="center"
+								spacing="xs"
+								style={{ pointerEvents: "none" }}
+							>
+								<Dropzone.Accept>
+									<IconUpload size={50} stroke={1.5} />
+								</Dropzone.Accept>
+								<Dropzone.Reject>
+									<IconX size={50} stroke={1.5} />
+								</Dropzone.Reject>
+								<Dropzone.Idle>
+									<IconPhoto size={50} stroke={1.5} />
+								</Dropzone.Idle>
 
-					<Group spacing="xs" position="apart">
-						<div>
-							<Group spacing="xs">
-								<Button
-									size="xs"
-									radius="xl"
-									color="blue"
-									leftIcon={<IconPhotoPlus size={14} />}
-									onClick={() => refOpenFile.current()}
-								>
-									Open
-								</Button>
-								<Button
-									size="xs"
-									radius="xl"
-									color="green"
-									leftIcon={<IconFileDownload size={14} />}
-									disabled={blobUrl === ""}
-									onClick={() => {
-										if (!refDownload.current || !refCanvas.current) return;
-										const a = refDownload.current;
-										a.href = refCanvas.current.toDataURL("image/png");
-										a.download = fileName.replace(/.svg$/, ".png");
-										a.click();
-									}}
-								>
-									Save
-								</Button>
-								<Button
-									size="xs"
-									radius="xl"
-									color="red"
-									leftIcon={<IconTrashX size={14} />}
-									disabled={blobUrl === ""}
-									onClick={() => {
-										setDimension("");
-										setBlobUrl("");
-									}}
-								>
-									Clear
-								</Button>
+								<div>
+									<Text size="xl" inline>
+										Drag svg here or click to select svg file
+									</Text>
+								</div>
 							</Group>
-						</div>
-						<Group spacing="xs">
-							<Text weight={700} size="xs">
-								{dimension}
-							</Text>
-							<ColorInput
-								size="xs"
-								radius="xl"
-								value={colorBg}
-								onChange={setColorBg}
-								style={{ width: "5.75rem" }}
+						</Dropzone>
+
+						<div
+							style={{
+								position: "relative",
+								display: svgBase64Url !== "" ? "block" : "none",
+								flexGrow: 1,
+								background: colorBg,
+							}}
+						>
+							<img
+								ref={refImage}
+								src={svgBase64Url}
+								style={{
+									position: "absolute",
+									top: 0,
+									left: 0,
+									width: "100%",
+									height: "100%",
+									objectFit: "scale-down",
+									objectPosition: "center",
+								}}
+								onLoad={onImageLoaded}
+								crossOrigin="anonymous"
+								draggable={false}
 							/>
+						</div>
+
+						<Group spacing="xs" position="apart">
+							<div>
+								<Group spacing="xs">
+									<Button
+										size="xs"
+										radius="xl"
+										color="blue"
+										leftIcon={<IconPhotoPlus size={14} />}
+										onClick={() => refOpenFile.current()}
+									>
+										Open
+									</Button>
+									<Button
+										size="xs"
+										radius="xl"
+										color="green"
+										leftIcon={<IconFileDownload size={14} />}
+										disabled={svgBase64Url === ""}
+										onClick={() => {
+											if (!refDownload.current || !refCanvas.current) return;
+											const a = refDownload.current;
+											a.href = refCanvas.current.toDataURL("image/png");
+											a.download = fileName.replace(/.svg$/, ".png");
+											a.click();
+										}}
+									>
+										Save
+									</Button>
+									<Button
+										size="xs"
+										radius="xl"
+										color="red"
+										leftIcon={<IconTrashX size={14} />}
+										disabled={svgBase64Url === ""}
+										onClick={() => {
+											setDimension("");
+											setSvgBase64Url("");
+										}}
+									>
+										Clear
+									</Button>
+								</Group>
+							</div>
+							<Group spacing="xs">
+								<Text weight={700} size="xs">
+									{dimension}
+								</Text>
+								<ColorInput
+									size="xs"
+									radius="xl"
+									value={colorBg}
+									onChange={setColorBg}
+									style={{ width: "5.75rem" }}
+								/>
+							</Group>
 						</Group>
-					</Group>
-				</Stack>
-			</Container>
+					</Stack>
+				</Container>
+			</NotificationsProvider>
 		</MantineProvider>
 	);
 };
